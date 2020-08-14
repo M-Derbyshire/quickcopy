b1603e0cc7c53b5c14a43d5b4d4a51496d14bb98
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-08-14 16:51:41 +0100

    setup the system to set the currently selected file

---
+++ batch_files/set_chosen_file_path.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+SET fullPath=%cd%\%1
+ECHO %fullPath% > %~dp0/chosen_file_path.txt
+ECHO File to copy/move is now %fullPath%
--- quickcopy.cmd
+++ quickcopy.cmd
@@ -4,14 +4,19 @@ REM Route the request to the correct batch file in ./batch_files
 IF "%1"=="" GOTO displayHelp
 
 
-IF /I "%1%"=="-h" GOTO displayHelp 
-IF /I "%1%"=="--help" GOTO displayHelp
-IF /I "%1%"=="/?" GOTO displayHelp
+IF /I "%1"=="/h" GOTO displayHelp 
+IF /I "%1"=="--help" GOTO displayHelp
+IF /I "%1"=="/?" GOTO displayHelp
 
-
+IF /I "%1"=="/s" GOTO setChosenPath
+IF /I "%1"=="--set" GOTO setChosenPath
 
 GOTO:eof
 
 :displayHelp
 CALL %~dp0/batch_files/display_help.bat
+GOTO:eof
+
+:setChosenPath
+IF NOT "%2"=="" CALL %~dp0/batch_files/set_chosen_file_path.bat %2
 GOTO:eof
