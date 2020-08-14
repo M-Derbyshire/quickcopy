3465e08b13e9603ab4cb855dcaf1e36c75c9e600
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-08-14 15:46:07 +0100

    added instructions for the emptying of the chosen file to the help display

--- batch_files/display_help.bat
+++ batch_files/display_help.bat
@@ -5,5 +5,6 @@ ECHO ---------
 ECHO A simpler way to copy and move files with your Windows command line.
 ECHO --------- 
 ECHO Set the file that will be moved or copied: /s ^<file^> ^|^| --set ^<file^>
+ECHO Clear the chosen file: /e ^|^| --empty
 ECHO Copy the chosen file: /c ^<new file path^> ^|^| --copy ^<new file path^>
 ECHO Move the chosen file: /m ^<new file path^> ^|^| --move ^<new file path^>
