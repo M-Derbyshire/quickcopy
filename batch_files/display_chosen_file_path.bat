53400a16e3aa68f711ce0e0794eeb9fd21ec5078
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-08-15 23:19:36 +0100

    display_chosen_file_path now displays the path on the same line as the message before it

--- batch_files/display_chosen_file_path.bat
+++ batch_files/display_chosen_file_path.bat
@@ -2,8 +2,9 @@
 
 SET fileWithChosenPath=%~dp0chosen_file_path.txt
 IF EXIST %fileWithChosenPath% (
-	ECHO The chosen file path to copy/move is:
-	TYPE %fileWithChosenPath%
+	FOR /F %%a IN (%fileWithChosenPath%) DO (
+		ECHO The chosen file path to copy/move is: %%a
+	)
 ) ELSE (
 	ECHO No file has been chosen
 ) 
